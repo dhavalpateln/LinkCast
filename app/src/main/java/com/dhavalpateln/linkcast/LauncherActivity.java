efb171aaa97370fcabe3037202693e8897ab760b
Dhaval <dhaval0024@gmail.com>
2021-03-29 19:33:56 -0700

    updated version

--- app/src/main/java/com/dhavalpateln/linkcast/LauncherActivity.java
+++ app/src/main/java/com/dhavalpateln/linkcast/LauncherActivity.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class LauncherActivity extends AppCompatActivity {
 
     private final String TAG = "LAUNCHER_ACTIVITY";
-    private final String APP_VERSION = "v3.1";
+    private final String APP_VERSION = "v3.1.1";
 
     private static final int RC_SIGN_IN = 9001;
 
